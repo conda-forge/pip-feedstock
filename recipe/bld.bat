bbc89f4e04379b39a89c790cd526954d3fc5210b
Ray Donnelly <mingw.android@gmail.com>
2020-02-21 19:03:57 +0100

    Debug build fixes

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,13 @@
-%PYTHON% setup.py install --single-version-externally-managed --record record.txt
-if errorlevel 1 exit 1
+if "%PY_INTERP_DEBUG%" neq "" (
+  %PYTHON% setup.py build -g install --single-version-externally-managed --record record.txt -v -v
+  if errorlevel 1 exit 1
+) else (
+  %PYTHON% setup.py build install --single-version-externally-managed --record record.txt -v -v
+  if errorlevel 1 exit 1
+)
 
 cd %SCRIPTS%
 del *.exe
 del *.exe.manifest
 del pip2*
 del pip3*
-:: del %SP_DIR%\__pycache__\pkg_res*
